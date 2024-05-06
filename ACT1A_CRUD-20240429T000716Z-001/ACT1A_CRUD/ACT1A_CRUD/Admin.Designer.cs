1af8598b83f204e38a2fbebba435fcc7343e59a8
KyleSanDiego <160980155+KyleSanDiego@users.noreply.github.com>
2024-05-06 09:24:36 +0800

    Add files via upload

---
+++ ACT1A_CRUD-20240429T000716Z-001/ACT1A_CRUD/ACT1A_CRUD/Admin.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ACT1A_CRUD
+{
+    partial class Admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 60F, FontStyle.Bold);
+            label1.Location = new Point(302, 156);
+            label1.Name = "label1";
+            label1.Size = new Size(195, 106);
+            label1.TabIndex = 0;
+            label1.Text = "Haii";
+            // 
+            // Admin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Name = "Admin";
+            Text = "Admin";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
